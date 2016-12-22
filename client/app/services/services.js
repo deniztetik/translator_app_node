1516cea11204670f1e61cc1e96926ed260d052d9
Deniz Tetik <deniztetik31@gmail.com>
2016-12-22 18:17:28 -0500

    [feat] Add services file with http request functionality

---
+++ client/app/services/services.js
@@ -0,0 +1,24 @@
+angular.module('translations.services', [])
+    .factory('Translations', function($http) {
+        var getAll = function() {
+            return $http({
+                method: 'GET',
+                url: '/api/translations'
+            }).then(function(resp) {
+                return resp.data;
+            });
+        };
+
+        var addTranslation = function(text) {
+            return $http({
+                method: 'POST',
+                url: '/api/translations',
+                data: {original_lang_text: text}
+            });
+        };
+
+        return {
+            getAll: getAll,
+            addTranslation: addTranslation
+        };
+    })
