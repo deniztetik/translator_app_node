5fc7aae5b08b6be2c84d14e81015b211c859e4cb
Deniz Tetik <deniztetik31@gmail.com>
2016-12-27 15:15:09 -0500

    [fix] Use built-in "googleapis" library to make request to translate

---
+++ config/config.example.js
@@ -0,0 +1,5 @@
+// rename this file to "config.example.js" and replace the string with your Google API key
+
+var GOOGLE_API_KEY = 'YOUR_API_KEY_HERE';
+
+module.exports = GOOGLE_API_KEY;
--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express    = require('express')
     , app        = express()
     , port       = process.env.PORT || 8080
     , bodyParser = require('body-parser')
-    , request    = require('request')
-    , translationController = require('./controllers/translationController');
-
+    , google     = require('googleapis')
+    , translate  = google.translate('v2')
+    , translationController = require('./controllers/translationController')
+    , GOOGLE_API_KEY = require('./config/config.js');
 
 // to make parsing requests easier ///
 app.use(bodyParser.json());
@@ -16,16 +17,13 @@ app.use(express.static(__dirname + '/static'));
 
 
 app.post('/api/translations', function(req, res) {
-  var options = {
-    url: 'https://translation.googleapis.com/language/translate/v2',
-    qs: { target: 'en', q: req.body.original_lang_text},
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: 'Bearer ya29.El_BA2n5r1voh-9HaQbYHdrYcLjMN99LdRrj2u_k9l7JrUkXHVU_xrxY8IdAGgplKCDdSfeqmBb9_Cj9SS_uoiiC_l7kfFY-N-fpTtO9vt64707MU-r4Fe67i8oQeo7gEg'
-    }
+  var params = {
+    target: 'en',
+    q: req.body.original_lang_text,
+    auth: GOOGLE_API_KEY
   };
-  request(options, function(error, response, body) {
-    var parsedBody = JSON.parse(body);
+  translate.translations.list(params, {}, function (err, response) {
+    var parsedBody = response;
     var original_lang = parsedBody.data.translations[0].detectedSourceLanguage;
     var eng_translation = parsedBody.data.translations[0].translatedText;
     var translation = {
@@ -34,31 +32,18 @@ app.post('/api/translations', function(req, res) {
       original_lang_text: req.body.original_lang_text
     };
     translationController.addTranslation(translation)
-      .then(function() {
+      .then(function () {
         res.send(translation);
       });
   });
 });
 
-  // request();
-//curl -s -k -H 'Content-Type: application/json' \
-// -H 'Authorization: Bearer ya29.El-8Aysqs5nB64_EUKeoJNSF34NMmIZ8bxYPaHuhxNIb8P6kYEgUhBLmhAjmzAOwf0-iKsbWeCjVIncACsbpntgiz4-a3zBieo1QHwkndot4W-Cj5uTtKtEY0LeZnTHc-w' \
-// 'https://translation.googleapis.com/language/translate/v2' \
-// -d @translate-request.json
-// });
-
 app.get('/api/translations', function(req, res) {
   translationController.getTranslations()
     .then(function(translations) {
       res.send(translations.map(translation => translation.attributes));
-      // console.log(translations);
     });
 });
-
-// redirect gets to root if address not found //
-// app.get('*', (req, res) => {
-//     res.redirect('/');
-// });
 
 app.listen(port);
 
